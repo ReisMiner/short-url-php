5e3c17939f3b9bb63edafc14cd2d1eb414cb9e3a
ReisMiner <r1miner@outlook.com>
2021-09-13 21:45:23 +0200

    delete unused js file

--- assets/copy.js
+++
@@ -1,9 +0,0 @@
-function copy(text) {
-    let input = document.createElement('textarea');
-    input.innerHTML = text;
-    document.body.appendChild(input);
-    input.select();
-    let result = document.execCommand('copy');
-    document.body.removeChild(input);
-    return result;
-}
